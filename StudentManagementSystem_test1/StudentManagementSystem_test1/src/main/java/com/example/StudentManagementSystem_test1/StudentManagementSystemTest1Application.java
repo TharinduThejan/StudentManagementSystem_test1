4038840e5f93407ed54108a284e9da45c8a65d82
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-04-18 15:49:52 +0530

    fix errors

--- StudentManagementSystem_test1/StudentManagementSystem_test1/src/main/java/com/example/StudentManagementSystem_test1/StudentManagementSystemTest1Application.java
+++ StudentManagementSystem_test1/StudentManagementSystem_test1/src/main/java/com/example/StudentManagementSystem_test1/StudentManagementSystemTest1Application.java
@@ -1,13 +1,25 @@
 package com.example.StudentManagementSystem_test1;
 
+import com.example.StudentManagementSystem_test1.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemTest1Application {
+public class StudentManagementSystemTest1Application implements CommandLineRunner {
+
+	@Autowired
+	private StudentRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemTest1Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// You can test repository actions here
+		// e.g., repository.save(new Student(...));
+		System.out.println("Student Management System started successfully!");
+	}
 }
