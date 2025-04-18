c207e56b2af0baea8dabd39d47dec23bf8e39157
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-04-18 15:36:55 +0530

    Update StudentController.java

--- StudentManagementSystem_test1/StudentManagementSystem_test1/src/main/java/com/example/StudentManagementSystem_test1/controller/StudentController.java
+++ StudentManagementSystem_test1/StudentManagementSystem_test1/src/main/java/com/example/StudentManagementSystem_test1/controller/StudentController.java
@@ -1,4 +1,40 @@
 package com.example.StudentManagementSystem_test1.controller;
 
+import com.example.StudentManagementSystem_test1.model.Student;
+import com.example.StudentManagementSystem_test1.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
+@Autowired
+    private StudentService service;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return service.getAllStudents();
+    }
+    @PostMapping
+    public Student save(@RequestBody Student student) {
+        return service.saveStudent(student);
+    }
+    @GetMapping("/{id}")
+    public Student getById(@PathVariable Long id) {
+        return service.getStudentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Student update(@PathVariable Long id, @RequestBody Student student) {
+        student.setId(id);
+        return service.saveStudent(student);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.deleteStudent(id);
+
+    }
 }
