388144387d028d53ca202189b78c220ee33d9814
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-04-22 16:58:59 +0530

    add tailwindcss

---
+++ StudentManagementSystem_test1_frontend/src/api.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080/api/students";
+
+export const getStudents = () => axios.get(BASE_URL);
+export const getStudentById = (id) => axios.get(`${BASE_URL}/${id}`);
+export const addStudent = (student) => axios.post(BASE_URL, student);
+export const updateStudent = (id, student) =>
+  axios.put(`${BASE_URL}/${id}`, student);
+export const deleteStudent = (id) => axios.delete(`${BASE_URL}/${id}`);
