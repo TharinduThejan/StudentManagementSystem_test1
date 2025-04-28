b5decb3078adfa94e74fa6bb4351116017f1a5e1
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-04-28 14:51:19 +0530

    change backend controller cross origin localhost 3000 to 5173

--- StudentManagementSystem_test1/StudentManagementSystem_test1/src/main/java/com/example/StudentManagementSystem_test1/controller/StudentController.java
+++ StudentManagementSystem_test1/StudentManagementSystem_test1/src/main/java/com/example/StudentManagementSystem_test1/controller/StudentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 public class StudentController {
 @Autowired
     private StudentService service;
