66048b42ad21ba64015c82cc9cdf74a398c6f5d4
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-05-07 01:24:36 +0530

    MessageController

--- StudentManagementSystem_test1/StudentManagementSystem_test1/src/main/java/com/example/StudentManagementSystem_test1/controller/MessageController.java
+++ StudentManagementSystem_test1/StudentManagementSystem_test1/src/main/java/com/example/StudentManagementSystem_test1/controller/MessageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Messages")
+@RequestMapping("/api/messages")
 @CrossOrigin(origins = "http://localhost:5173")
 public class MessageController {
     @Autowired
